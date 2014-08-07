7115f6fa179cae840e45b55a25cb42d6f32e084d
Samuel Damashek <samuel.damashek@gmail.com>
2014-08-07 15:53:12 -0400

    base64 and common

---
+++ ciphers/base64.py
@@ -0,0 +1,5 @@
+def decrypt(ciphertext, key):
+    pass
+
+def crack(ciphertext):
+    pass
--- ciphers/common.py
+++ ciphers/common.py
@@ -1,13 +1,3 @@
-class Cipher:
-    def __init__(self):
-        raise NotImplementedError()
-
-    def decrypt(self, ciphertext, key):
-        raise NotImplementedError("The decrypt function is not implemented for this cipher.")
-
-    def crack(self, ciphertext):
-        raise NotImplementedError("The crack function is not implemented for this cipher.")
-
 def shift_string(string, offset):
     if offset == 0:
         return string
